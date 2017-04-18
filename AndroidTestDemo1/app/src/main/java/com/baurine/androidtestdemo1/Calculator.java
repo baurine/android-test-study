478dbcbce68a5523cff0c5fdc9ea7e393fe9e743
baurine <2008.hbl@gmail.com>
2017-04-18 14:41:10 +0800

    Init unit test

---
+++ AndroidTestDemo1/app/src/main/java/com/baurine/androidtestdemo1/Calculator.java
@@ -0,0 +1,23 @@
+package com.baurine.androidtestdemo1;
+
+/**
+ * Created by baurine on 4/18/17.
+ */
+
+public class Calculator {
+    public double sum(double a, double b) {
+        return 0;
+    }
+
+    public double substract(double a, double b) {
+        return 0;
+    }
+
+    public double divide(double a, double b) {
+        return 0;
+    }
+
+    public double multiply(double a, double b) {
+        return 0;
+    }
+}
