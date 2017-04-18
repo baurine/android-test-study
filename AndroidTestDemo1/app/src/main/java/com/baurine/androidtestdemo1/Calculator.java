cacf3d55a341c80160c5b3d0a7173e8e9d28f563
baurine <2008.hbl@gmail.com>
2017-04-18 14:42:02 +0800

    Make unit test pass

--- AndroidTestDemo1/app/src/main/java/com/baurine/androidtestdemo1/Calculator.java
+++ AndroidTestDemo1/app/src/main/java/com/baurine/androidtestdemo1/Calculator.java
@@ -6,18 +6,18 @@ package com.baurine.androidtestdemo1;
 
 public class Calculator {
     public double sum(double a, double b) {
-        return 0;
+        return a + b;
     }
 
     public double substract(double a, double b) {
-        return 0;
+        return a - b;
     }
 
     public double divide(double a, double b) {
-        return 0;
+        return a / b;
     }
 
     public double multiply(double a, double b) {
-        return 0;
+        return a * b;
     }
 }
